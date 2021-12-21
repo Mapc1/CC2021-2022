f770efbeefcb6ac626294493df72825eab629857
Mapc1 <marcocosta579@gmail.com>
2021-12-21 21:04:51 +0000

    Remove comments in FFSync

--- TP2/tp2/src/main/java/com/cc/ffsync/FFSync.java
+++ TP2/tp2/src/main/java/com/cc/ffsync/FFSync.java
@@ -13,24 +13,18 @@ import com.cc.ffsync.utils.ListWrapper;
 public class FFSync 
 {
     public static final String LOG_FOLDER = System.getProperty("user.home") + "/logs";
-    //public static final String LOG_FOLDER =  "logs";
     public static String SYNC_FOLDER;
 
     public static ListWrapper LW  = new ListWrapper();
 
     public static void main( String[] args ) throws IOException {
         Thread server;
-        //Thread client;
 
         FFSync.SYNC_FOLDER = args[0];
-//        InetAddress ip = InetAddress.getByName(args[1]);
         List<Thread> threads = new ArrayList<>();
 
         try {
             server = new Thread(new Server());
-            //client = new Thread(new Client(ip));
-
-//            client.start();
             server.start();
 
             for(int i = 1; i < args.length; i++) {
@@ -45,7 +39,6 @@ public class FFSync
             }
             threads = new ArrayList<>();
 
-//            client.join();
             server.join();
         } catch (NumberFormatException | SocketException e) {
             e.printStackTrace();
